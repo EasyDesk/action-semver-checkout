71e6d350fd10b732ae83cd0f10fa6341325fb728
francescodente <dente.francesco1996@gmail.com>
2021-09-11 15:19:37 +0200

    Create git-compute-version.sh

---
+++ git-compute-version.sh
@@ -0,0 +1,36 @@
+DESCRIBE_REGEX='^(.+)-(.+)-g(.+)$'
+SEMVER_REGEX='^v([1-9]\d*|0).([1-9]\d*|0).([1-9]\d*|0)$'
+IS_DEV_VERSION="true"
+
+DESCRIBE_RES=`git describe --long --abbrev=8 --match 'v[0-9]*.[0-9]*.[0-9]*' 2> /dev/null`
+if [ $? != 0 ] ; then
+  COMMIT_DISTANCE=`git rev-list --count HEAD`
+  COMMIT_HASH=`git log -n1 --format=%h`
+  VERSION="0.1.0"
+elif [[ "${DESCRIBE_RES}" =~ ${DESCRIBE_REGEX} ]] ; then
+  TAG=${BASH_REMATCH[1]}
+  COMMIT_DISTANCE=${BASH_REMATCH[2]}
+  COMMIT_HASH=${BASH_REMATCH[3]}
+
+  if [[ "$TAG" =~ $SEMVER_REGEX ]] ; then
+    MAJOR=${BASH_REMATCH[1]}
+    MINOR=${BASH_REMATCH[2]}
+    PATCH=${BASH_REMATCH[3]}
+    VERSION="${MAJOR}.${MINOR}.${PATCH}"
+    if [ ${COMMIT_DISTANCE} == 0 ] ; then
+      IS_DEV_VERSION="false"
+    fi
+  else
+    VERSION="${TAG}"
+  fi
+else
+  echo "::error ::Git describe returned an invalid description"
+  exit 1
+fi
+
+if [ $IS_DEV_VERSION == 'true' ] ; then
+  VERSION="${VERSION}-dev.${COMMIT_DISTANCE}+${COMMIT_HASH}"
+fi
+
+echo "::set-output name=version::${VERSION}"
+echo "::set-output name=is-dev-version::${IS_DEV_VERSION}"
