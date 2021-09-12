6ef84effb208011fe884b2c18e44a3227c1578ff
Francesco Dente <dente.francesco1996@gmail.com>
2021-09-12 15:57:22 +0200

    Fix version regex for bash

--- git-compute-version.sh
+++ git-compute-version.sh
@@ -1,27 +1,30 @@
-DESCRIBE_REGEX='^(.+)-(.+)-g(.+)$'
-SEMVER_REGEX='^v([1-9]\d*|0)\.([1-9]\d*|0)\.([1-9]\d*|0)$'
 IS_DEV_VERSION="true"
-
 DESCRIBE_RES=`git describe --long --abbrev=8 --match 'v[0-9]*.[0-9]*.[0-9]*' 2> /dev/null`
 if [ $? != 0 ] ; then
   COMMIT_DISTANCE=`git rev-list --count HEAD`
   COMMIT_HASH=`git log -n1 --format=%h`
   VERSION="0.1.0"
-elif [[ "${DESCRIBE_RES}" =~ ${DESCRIBE_REGEX} ]] ; then
+elif [[ "${DESCRIBE_RES}" =~ ^(.+)-(.+)-g(.+)$ ]] ; then
   TAG=${BASH_REMATCH[1]}
   COMMIT_DISTANCE=${BASH_REMATCH[2]}
   COMMIT_HASH=${BASH_REMATCH[3]}
 
-  if [[ "${TAG}" =~ ${SEMVER_REGEX} ]] ; then
+  echo "tag $TAG"
+  echo "commit distance $COMMIT_DISTANCE"
+  echo "commit hash $COMMIT_HASH"
+
+  if [[ "${TAG}" =~ ^v([1-9][0-9]*|0)\.([1-9][0-9]*|0)\.([1-9][0-9]*|0)$ ]] ; then
     MAJOR=${BASH_REMATCH[1]}
     MINOR=${BASH_REMATCH[2]}
     PATCH=${BASH_REMATCH[3]}
     VERSION="${MAJOR}.${MINOR}.${PATCH}"
+    echo "is sem ver"
 
     if [ ${COMMIT_DISTANCE} == 0 ] ; then
       IS_DEV_VERSION="false"
     fi
   else
+    echo "not sem ver"
     VERSION="${TAG}"
   fi
 else
